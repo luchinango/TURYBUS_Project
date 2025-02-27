f30a98e2bed5878d47a64ad38dbaa9e44ab5bef7
Nicolas Oporto <noportor@soe.uagrm.edu.bo>
2025-02-27 15:10:33 -0400

    Moviendo scripts base a carpeta por noportor

---
+++ base_scripts/Scripts/DDL/05_Pasajeros.sql
@@ -0,0 +1,5 @@
+USE TURYBUS_DB;
+GO
+CREATE TABLE Operaciones.Pasajeros (     id_pasajero INT IDENTITY(1,1) PRIMARY KEY,     DNI VARCHAR(20) NOT NULL UNIQUE,     nombre VARCHAR(50) NOT NULL,     apellidos VARCHAR(50) NOT NULL,     telefono VARCHAR(25) 
+); 
+GO 
