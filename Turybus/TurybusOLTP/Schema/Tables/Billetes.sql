d9fd224e2a440ae1e528f5681c649dd00016b84a
Nicolas Oporto <noportor@soe.uagrm.edu.bo>
2025-02-27 14:48:31 -0400

    Agregando solucion en visual studio por noportor

---
+++ Turybus/TurybusOLTP/Schema/Tables/Billetes.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE Operaciones.Billetes (
+    id_billete INT IDENTITY(1,1) PRIMARY KEY,
+    id_servicio INT NOT NULL,
+    id_pasajero INT NOT NULL,
+    id_empleado INT NULL,
+    fecha DATE NOT NULL,
+    importe DECIMAL(10,2) NOT NULL,
+    hora_salida TIME NOT NULL,
+    hora_llegada TIME NOT NULL,
+
+    CONSTRAINT FK_Billete_Servicio FOREIGN KEY (id_servicio) 
+        REFERENCES Turismo.ServiciosDiarios(id_servicio),
+    CONSTRAINT FK_Billete_Pasajero FOREIGN KEY (id_pasajero) 
+        REFERENCES Operaciones.Pasajeros(id_pasajero),
+    CONSTRAINT FK_Billete_Empleado FOREIGN KEY (id_empleado)
+        REFERENCES Operaciones.Empleados(id_empleado)
+);
---
+++ Turybus/TurybusOLTP/Schema/Tables/Paradas.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE Turismo.Paradas (
+ id_parada INT IDENTITY(1,1) PRIMARY KEY,
+ id_ruta INT NOT NULL,
+ orden INT NOT NULL, -- Orden de la parada en el itinerario de viaje
+ nombre_lugar VARCHAR(100) NOT NULL,
+ hora_prevista TIME NOT NULL,
+ actividad VARCHAR(100), -- Ejemplo: comida, visita, etc.
+ tiempo_parada TIME, -- Duración de la actividad
+ CONSTRAINT FK_Parada_Ruta FOREIGN KEY (id_ruta)
+ REFERENCES Turismo.Rutas(id_ruta)
+);
