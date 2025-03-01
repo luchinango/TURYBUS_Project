26d54d46f659fd7460fda79eb267bc41b315d278
Nicolas Oporto <noportor@soe.uagrm.edu.bo>
2025-03-01 17:46:30 -0400

    Agregando script datos maestros en OLTP y agregando solucion para DW

---
+++ TurybusDW/TurybusDW/Schema/Tables/Dim_Paradas.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Dim_Paradas (
+    ParadaSK INT IDENTITY(1,1) PRIMARY KEY,  -- Surrogate Key
+    ParadaID INT NOT NULL,                   -- Business Key
+    NombreLugar VARCHAR(100) NOT NULL,
+    HoraPrevista TIME NOT NULL,
+    Actividad VARCHAR(100),
+    TiempoParada TIME
+)
