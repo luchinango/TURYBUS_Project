65cc8f395dacb1405a04732b59018389748c543e
Luis Alberto <luisalbertomartinezbarrientos@gmail.com>
2025-02-28 21:55:32 -0400

    Organizo archivos, renombro scripts y agrego documentación

---
+++ Turybus/TurybusOLTP/Schema/Tables/Revisiones.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Seguridad].[Revisiones] (
+    [id_revision]       INT           IDENTITY (1, 1) NOT NULL,
+    [matricula_autobus] VARCHAR (20)  NOT NULL,
+    [fecha_revision]    DATE          NOT NULL,
+    [diagnostico]       VARCHAR (500) NOT NULL,
+    PRIMARY KEY CLUSTERED ([id_revision] ASC),
+    CONSTRAINT [FK_Revisiones_Autobus] FOREIGN KEY ([matricula_autobus]) REFERENCES [Operaciones].[Autobuses] ([matricula])
+);
